fd9df7cac1c3416019c7d7180e62364d9b12b5ca
--replace-all <bulgan.suzan@gmail.com>
2023-11-29 16:07:50 +0100

    last change

--- character/src/App.js
+++ character/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from "react";
+import Card from "./components/Card";
+import data from "./assets/data";
 
-function App() {
+const initialValue = {
+  textEntered: "",
+  deckFilter: null,
+};
+
+const reducer = function (state, action) {
+  switch (action.type) {
+    case "onChangeInput": // if(action.type === 'onChangeInput')
+      const searchFilter = data.filter((item) =>
+        item.name.toLowerCase().includes(action.payload.toLowerCase())
+      );
+
+      return {
+        ...state,
+        textEntered: action.payload,
+        deckFilter: searchFilter,
+      };
+
+    default:
+      break;
+  }
+};
+
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialValue);
+
+  const onChangeHandler = (event) =>
+    dispatch({ type: "onChangeInput", payload: event.target.value });
+
+  const decks = state.deckFilter || data;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="p-10">
+      <h1 className="text-2xl text-gray-100 text-center mb-10 uppercase font-mono">
+        Characters ⚡️
+      </h1>
+
+      <input
+        onChange={onChangeHandler}
+        type="search"
+        className="w-56 mb-8 px-2 rounded py-3"
+        placeholder="Search Character "
+      />
+
+      <div className="grid md:grid-cols-3 gap-5 ">
+        {decks.map((item) => (
+          <Card
+            key={item.name}
+            image={item.image}
+            name={item.name}
+            description={item.description}
+          />
+        ))}
+      </div>
+    </main>
   );
 }
-
-export default App;
